bc77eace30e722b6a1493e9c199ed6185e57bb9a
EvieZhou <87358629+EvieZhou@users.noreply.github.com>
2025-05-07 15:53:53 -0700

    Update one_page_website.js

--- One-Page Website/js/one_page_website.js
+++ One-Page Website/js/one_page_website.js
@@ -1,1 +1,36 @@
+// Modal variables
+var modal = document.getElementById("myModal");
+var slideIndex = 1;
+
+// Open the modal
+function openModal() {
+  modal.style.display = "block";
+  showSlides(slideIndex);
+}
+
+// Close the modal
+function closeModal() {
+  modal.style.display = "none";
+}
+
+// Show specific slide
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+// Show slides
+function showSlides(n) {
+  var slides = document.getElementsByClassName("mySlides");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (var i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slides[slideIndex-1].style.display = "block";
+}
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
 
